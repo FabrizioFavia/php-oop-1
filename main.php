a3bae8f693b5da3b519dc9540271a5c34d7a84e9
FabrizioFavia <personal.favia@gmail.com>
2023-06-20 17:13:56 +0200

    feat(main.php): created $genre Array

--- main.php
+++ main.php
@@ -13,25 +13,24 @@ class Movie
         $this->title = $title;
         $this->director = $director;
         $this->releaseDate = $releaseDate;
-        $this->genre = $genre;
-        
+        $this->genre = [$genre];
     }
 
     public function setVote($vote)
-    { if (!is_nan($vote) and ($vote > 0 and $vote <= 5) ) {
-        return $this->vote = "Voto". ": ". $vote;
-    } else{
-        return null;
-    }
-        
+    {
+        if (!is_nan($vote) and ($vote > 0 and $vote <= 5)) {
+            return $this->vote = "Voto" . ": " . $vote;
+        } else {
+            return null;
+        }
     }
 }
 
 $theNest = new Movie("The nest", "Roberto De Feo", "09/08/2019", "Thriller");
 $theNest->setVote(5);
-$fightClub = new Movie("Fight Club", "David Fincher", "02/07/2003", "Drammatico");
+$fightClub = new Movie("Fight Club", "David Fincher", "02/07/2003", ["Drammatico", "thriller"]);
 $fightClub->setVote(5);
-$aceVentura = new Movie("Ace Ventura", "Steve Oedekerk", "22/04/1995", "Commedia");
+$aceVentura = new Movie("Ace Ventura", "Steve Oedekerk", "22/04/1995", ["Commedia", "Avventura"]);
 $aceVentura->setVote(2);
 
 
@@ -50,28 +49,44 @@ $aceVentura->setVote(2);
 
 <body>
     <div class="movieContainer d-flex">
+        <!-- MOVIE CARD -->
         <div class="movieCard m-4 border border-success d-flex flex-column justify-content-center align-items-center">
-            <?php foreach ($fightClub as $item) { ?>
-
-                <div class="mb-2"><?php echo ($item) ?></div>
-
-            <?php } ?>
+            <div class="mb-2"><strong>Titolo:</strong> <?php echo $theNest->title; ?></div>
+            <div class="mb-2"><strong>Regista:</strong> <?php echo $theNest->director; ?></div>
+            <div class="mb-2"><strong>Data di uscita:</strong> <?php echo $theNest->releaseDate; ?></div>
+            <div class="mb-2"><strong>Generi:</strong>
+                <?php foreach ($theNest->genre as $genre) { ?>
+                    <?php echo $genre; ?>
+                <?php } ?>
+            </div>
+            <div class="mb-2"><strong>Voto:</strong> <?php echo $theNest->vote; ?></div>
         </div>
+        <!-- MOVIE CARD -->
         <div class="movieCard m-4 border border-success d-flex flex-column justify-content-center align-items-center">
-            <?php foreach ($theNest as $item) { ?>
-
-                <div class="mb-2"><?php echo ($item) ?></div>
-
-            <?php } ?>
+            <div class="mb-2"><strong>Titolo:</strong> <?php echo $fightClub->title; ?></div>
+            <div class="mb-2"><strong>Regista:</strong> <?php echo $fightClub->director; ?></div>
+            <div class="mb-2"><strong>Data di uscita:</strong> <?php echo $fightClub->releaseDate; ?></div>
+            <div class="mb-2"><strong>Generi:</strong>
+                <?php foreach ($fightClub->genre as $genre) { ?>
+                    <?php echo implode(", ", $genre); ?>
+                <?php } ?>
+            </div>
+            <div class="mb-2"><strong>Voto:</strong> <?php echo $fightClub->vote; ?></div>
         </div>
+        <!-- MOVIE CARD -->
         <div class="movieCard m-4 border border-success d-flex flex-column justify-content-center align-items-center">
-            <?php foreach ($aceVentura as $item) { ?>
-
-                <div class="mb-2"><?php echo ($item) ?></div>
-
-            <?php } ?>
+            <div class="mb-2"><strong>Titolo:</strong> <?php echo $aceVentura->title; ?></div>
+            <div class="mb-2"><strong>Regista:</strong> <?php echo $aceVentura->director; ?></div>
+            <div class="mb-2"><strong>Data di uscita:</strong> <?php echo $aceVentura->releaseDate; ?></div>
+            <div class="mb-2"><strong>Generi:</strong>
+                <?php foreach ($aceVentura->genre as $genre) { ?>
+                    <?php echo implode(", ", $genre); ?>
+                <?php } ?>
+            </div>
+            <div class="mb-2"><strong>Voto:</strong> <?php echo $aceVentura->vote; ?></div>
         </div>
     </div>
+
 </body>
 
 </html>
